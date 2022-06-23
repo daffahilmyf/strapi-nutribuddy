2a9fc563762457f736f44a185f9131e2a8cf33b8
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2022-06-23 10:24:27 +0700

    add configuration

--- config/server.js
+++ config/server.js
@@ -1,7 +1,15 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env("https://nutribuddy.herokuapp.com"),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
 });
