520e4f50f208bbadcd36febdff882dc750b0ea68
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2022-06-27 16:11:32 +0700

    change the configuration

--- config/server.js
+++ config/server.js
@@ -1,15 +1,8 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-// });
-
 module.exports = ({ env }) => ({
-  url: env("https://nutribuddy.herokuapp.com"),
-  proxy: true,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array('APP_KEYS'),
   },
 });
+
